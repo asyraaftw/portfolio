316e91222504c64b87167ab2b21c1953fabdc032
Amirul Asyraaf <asyraafchy@gmail.com>
2025-04-08 02:51:53 +0800

    remove unused

--- src/components/AppAppBar.tsx
+++ src/components/AppAppBar.tsx
@@ -15,28 +15,6 @@ interface AppBarProps {
 export default function AppAppBar({ color }: AppBarProps) {
   const pages = ["about", "experience", "projects"];
 
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
     <AppBar position="static" sx={{ backgroundColor: color }}>
       <Container maxWidth="xl">
@@ -44,11 +22,7 @@ export default function AppAppBar({ color }: AppBarProps) {
           <Box>
             {pages?.map((x, i) => (
               <>
-                <Button
-                  key={i}
-                  onClick={handleCloseNavMenu}
-                  sx={{ color: "white" }}
-                >
+                <Button key={i} sx={{ color: "white" }}>
                   {x}
                 </Button>
               </>
